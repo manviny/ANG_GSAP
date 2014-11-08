6547fd0c2727800a5e9873e7d175ef526c617e3b
manviny <manol@mac-2.home>
2014-11-08 11:35:14 +0100

    slide fade y titulo se mueve

--- app/scripts/controllers/slidefade.js
+++ app/scripts/controllers/slidefade.js
@@ -13,18 +13,28 @@ angular.module('tweenLiteApp')
 
 	
 	var $slides = $(".slide");			//slides
+	var $titulos = $(".titulo");		//slides
 	var currentSlide = 0;				//keep track on the current slide
 	var stayTime = 3;					//time the slide stays
 	var slideTime = 1.3;				//fade in / fade out time
 				
 	TweenLite.set($slides.filter(":gt(0)"), {autoAlpha:0});	//we hide all images after the first one
+	TweenLite.set($titulos.filter(":gt(0)"), {autoAlpha:0});	//we hide all images after the first one
 	TweenLite.delayedCall(stayTime, nextSlide);				//start the slideshow
 				
-	function nextSlide(){					
+	function nextSlide(){			
+
 			TweenLite.to( $slides.eq(currentSlide), slideTime, {autoAlpha:0} );		//fade out the old slide
+			TweenLite.to( $titulos.eq(currentSlide), slideTime, {left:-500, autoAlpha:0.5} );		//fade out the old slide
+
 			currentSlide = ++currentSlide % $slides.length;							//find out the next slide			
+			
 			TweenLite.to( $slides.eq(currentSlide), slideTime, {autoAlpha:1} );		//fade in the next slide
+			TweenLite.to( $titulos.eq(currentSlide), slideTime, {left:200, autoAlpha:1} );		//fade in the next slide
+			
 			TweenLite.delayedCall(stayTime, nextSlide);								//wait a couple of seconds before next slide
+	
+
 	}
 
 
